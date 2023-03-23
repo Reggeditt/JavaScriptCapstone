3c4a64e4b2ddab65ae903661ed3632878776621a
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-03-23 13:51:36 +0100

    Add #14 display comments pop up with selected item's details - Student B

--- src/index.js
+++ src/index.js
@@ -4,28 +4,13 @@ const apiBaseUrl = 'https://api.tvmaze.com';
 const involvementAPIBaseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
 const searchFormEl = document.getElementById('search-form');
 const searchInputEl = document.getElementById('search-input');
-const involvementApiID = window.localStorage.getItem('involvementApiID');
-
-const createInvolvementTrackerApp = () => {
-  fetch(`${involvementAPIBaseUrl}apps/`, {
-    method: 'POST',
-  })
-    .then((result) => {
-      if (result.ok) {
-        return result.text();
-      }
-      throw new Error('there was a problem');
-    })
-    .then((data) => {
-      window.localStorage.setItem('involvementApiID', data);
-    });
-};
+const involvementApiID = '71VEvRYFUclLOrVdlfg1';
 
 const updateLikesCounter = async (id) => {
-  const getLikes = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${involvementApiID}/likes`);
+  const getLikes = await fetch(`${involvementAPIBaseUrl}apps/${involvementApiID}/likes`);
   const likesData = await getLikes.json();
 
-  const counter = document.getElementById(`${id}-counter`);
+  const counter = document.getElementById(`${id} - counter`);
   const likesCount = likesData.filter((like) => like.item_id === id);
   counter.textContent = (`${likesCount[0].likes + 1} likes`);
 };
@@ -43,9 +28,60 @@ const postLike = async (id) => {
 };
 
 const searchShows = async (query) => {
-  const response = await fetch(`${apiBaseUrl}/search/shows?q=${query}`);
+  const response = await fetch(`${apiBaseUrl}/search/shows ? q = ${query}`);
   const data = await response.json();
   return data;
+};
+
+const closePopup = (id) => {
+  const closeBtns = document.querySelectorAll('.popup-close-btn');
+  const commentsPopupWrapperEl = document.querySelector('.comments-popup-wrap');
+  const commentsPopupEl = document.querySelector(`#popup${id}`);
+  closeBtns.forEach((closeBtn) => {
+    closeBtn.addEventListener('click', () => {
+      commentsPopupEl.remove();
+      commentsPopupWrapperEl.style.display = 'none';
+    });
+  });
+};
+
+const renderCommentsPopup = (data) => {
+  const commentsPopupWrapperEl = document.querySelector('.comments-popup-wrap');
+  const commentsPopupEl = document.createElement('div');
+  commentsPopupEl.classList.add('comments-popup');
+  commentsPopupEl.id = `popup${data.id}`;
+  commentsPopupEl.innerHTML = `
+    <div class="popup-header">
+      <img class='popup-image' src='${data.image.original}'></img>
+      <span class='popup-close-btn'>&times;</span>
+    </div>
+    <div class='popup-content'>
+      <h2 class='title'>${data.name}</h2>
+      <div class='details'>
+        <div class='pop-show-header'>
+          <div class='pop-show-genre'>
+            <p class='show-genre'>Genres: ${data.genres}</p>
+            <p class='show-rating'>Rating: ${data.rating.average}</p>
+          </div>
+          <div class='pop-runtime-header'>
+            <p class='show-runtime'>Runtime: ${data.runtime}mins</p>
+            <p class='show-status'>Status: ${data.status}</p>
+          </div>
+        </div>
+        <p class='show-language'>Language: ${data.language}</p>
+        <p class='show-synopsis'>${data.summary}</p>
+      </div>
+    </div>
+    <h3 class='comments-title'>Comments (0)</h3>
+    <div class='popup-comments'></div>
+    <form class='popup-comment-form' id='popup-comment-form'>
+      <input type='name' name='name' placeholder='Your name' required>
+      <textarea name='comment' placeholder='Your comment' required></textarea>
+      <button type='submit'>Submit</button>
+    </form>
+  `;
+  commentsPopupWrapperEl.appendChild(commentsPopupEl);
+  closePopup(data.id);
 };
 
 const renderShow = (data) => {
@@ -65,6 +101,15 @@ const renderShow = (data) => {
     <p class='rent-movie-button' data-id='${data.id}'>Rent movie</p>
   `;
   mainContentWrapperEl.appendChild(showCardEl);
+  const commentBtn = document.querySelector(`#show${data.id}`);
+  commentBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    renderCommentsPopup(data);
+    const commentsPopupWrapperEl = document.querySelector('.comments-popup-wrap');
+    commentsPopupWrapperEl.style.display = 'flex';
+    const commentsPopupEl = document.querySelector(`#popup${data.id}`);
+    commentsPopupEl.style.display = 'flex';
+  });
   updateLikesCounter(`item${data.id}`);
 };
 
@@ -79,7 +124,7 @@ const renderShow = (data) => {
 //     }),
 //   };
 
-//   fetch(`${involvementAPIBaseUrl}apps/${involvementApiID}/comments/`, options)
+//   fetch(${involvementAPIBaseUrl}apps/${involvementApiID}/comments/, options)
 //     .then((response) => {
 //       if (response.ok) console.log(response.status);
 //       throw new Error('there was a problem');
@@ -137,39 +182,3 @@ searchFormEl.addEventListener('submit', async (event) => {
     });
   });
 });
-
-if (!window.localStorage.getItem('involvementApiID')) createInvolvementTrackerApp();
-
-// // const renderCommentsPopup = (data) => {
-// //   console.log('rendering comments popup...', data);
-// //   const commentsPopupWrapperEl = document.querySelector('.comments-popup-wrap');
-// //   // commentsPopupWrapperEl.innerHTML = '';
-// //   const commentsPopupEl = document.createElement('div');
-// //   commentsPopupEl.classList.add('comments-popup');
-// //   commentsPopupEl.innerHTML = `
-// //     <img class='popup-image' src='${data.image.original}'></img>
-// //     <div class='popup-content'>
-// //       <h2 class='title'>${data.name}</h2>
-// //       <div class='details'>
-// //       <p class='show-genre'>${data.genres}</p>
-// //       <p class='show-rating'>${data.rating.average}</p>
-// //       <p class='show-runtime'>${data.runtime}</p>
-// //       <p class='show-status'>${data.status}</p>
-// //       <p class='show-language'>${data.language}</p>
-// //       <p class='show-synopsis'>${data.summary}</p>
-// //       </div>
-// //       <div class='likes-info'>
-// //         <div class='like-button'></div>
-// //         <small class='likes-counter'>0 likes</small>
-// //       </div>
-// //     </div>
-// //     <h3 class='comments-title'>Comments (0)</h3>
-// //     <div class='comments'></div>
-// //     <form class='popup-comment-form' id='popup-comment-form'>
-// //       <input type='name' name='name' placeholder='Your name' required>
-// //       <textarea name='comment' placeholder='Your comment' required></textarea>
-// //       <button type='submit'>Submit</button>
-// //     </form>
-// //   `;
-// //   commentsPopupWrapperEl.appendChild(commentsPopupEl);
-// // };
