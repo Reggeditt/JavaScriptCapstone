ad3e4236bc27665fa6934591ff8901c0e656f810
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-03-24 15:50:34 +0100

    fix linters error

--- src/modules/__MOCK__/countComments.js
+++ src/modules/__MOCK__/countComments.js
@@ -1,7 +1,7 @@
 const commentsCounter = (targetEl) => {
-    const ourArray = document.querySelectorAll(`${targetEl}`);
-    const count = ourArray.length;
-    return count;
+  const ourArray = document.querySelectorAll(`${targetEl}`);
+  const count = ourArray.length;
+  return count;
 };
 
 module.exports = commentsCounter;
